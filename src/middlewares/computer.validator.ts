fd8db22ced3c5f987a26200d29326faa47263631
PDJ6975 <antrodcal@alum.us.es>
2026-05-17 19:07:53 +0200

    fix: cambiar error en el health hacia computer y registros de errores como error en log

--- src/middlewares/computer.validator.ts
+++ src/middlewares/computer.validator.ts
@@ -1,12 +1,12 @@
 import { type Request, type Response, type NextFunction } from 'express';
-import { ValidationError } from '../utils/customErrors.js';
+import { ExternalServiceError } from '../utils/customErrors.js';
 import * as computerIntegration from '../integrations/computer.integration.js';
 
 export const validateComputerHealth = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const computerHealth = await computerIntegration.checkHealth();
         if (!computerHealth) {
-            return next(new ValidationError('Computer service is not available'));
+            return next(new ExternalServiceError('Computer service is not available'));
         }
         next();
     } catch (err) {
